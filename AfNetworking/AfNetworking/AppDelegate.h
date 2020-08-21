8c93fc7f74a3d9f42eaff94ca63d62700c373352
Gayatri-26 <69888188+Gayatri-26@users.noreply.github.com>
2020-08-21 14:37:16 +0530

    Add AfNetworking folder

---
+++ AfNetworking/AfNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AfNetworking
+//
+//  Created by Mac on 20/08/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AfNetworking/AfNetworking/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  AfNetworking
+//
+//  Created by Mac on 20/08/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AFNetworking/AFNetworking.h"
+
+@interface ViewController : UIViewController
+
+
+@end
+
