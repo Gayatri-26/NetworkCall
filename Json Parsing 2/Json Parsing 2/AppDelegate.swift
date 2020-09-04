b86fdafda5e622d4a0d84955b4d4d51f193cc3d2
Sonali Nivangune <nivangunesonali19@gmail.com>
2020-09-04 15:24:16 +0530

    changes made to appdelegatefile

--- Json Parsing 2/Json Parsing 2/AppDelegate.swift
+++ Json Parsing 2/Json Parsing 2/AppDelegate.swift
@@ -21,10 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
    // let FVC = storyboard.instantiateViewController(withIdentifier:"FirstVC")
   //  window?.rootViewController = FVC
-  //    let VC = storyboard.instantiateViewController(withIdentifier:"NVC")
-  //    window?.rootViewController = VC
-    let SVC = storyboard.instantiateViewController(withIdentifier:"SecondVC")
-    window?.rootViewController = SVC
+     let VC = storyboard.instantiateViewController(withIdentifier:"NVC")
+      window?.rootViewController = VC
+  //  let SVC = storyboard.instantiateViewController(withIdentifier:"SecondVC")
+  //  window?.rootViewController = SVC
       window?.makeKeyAndVisible()
        
         return true
