470ee14cd37968be0555e4214576c030c39c8569
Gayatri-26 <69888188+Gayatri-26@users.noreply.github.com>
2020-09-07 16:15:08 +0530

    Delete the new files

--- Json Parsing 2/Json Parsing 2/AppDelegate.swift
+++ Json Parsing 2/Json Parsing 2/AppDelegate.swift
@@ -20,8 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         
         
-        //    let FVC = storyboard.instantiateViewController(withIdentifier:"FirstVC")
-        //    window?.rootViewController = FVC
+            let FVC = storyboard.instantiateViewController(withIdentifier:"FirstVC")
+            window?.rootViewController = FVC
         
         //     let VC = storyboard.instantiateViewController(withIdentifier:"NVC")
         //      window?.rootViewController = VC
@@ -29,8 +29,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //  let SVC = storyboard.instantiateViewController(withIdentifier:"SecondVC")
         //  window?.rootViewController = SVC
         
-        let NSDispatchQueue = storyboard.instantiateViewController(withIdentifier: "NsurlUsingDispatchQueue")
-        window?.rootViewController = NSDispatchQueue
         
         window?.makeKeyAndVisible()
         
