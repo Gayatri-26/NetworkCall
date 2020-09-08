ad95e2bf6fde09205faf51ce976936532c0adee6
Gayatri-26 <69888188+Gayatri-26@users.noreply.github.com>
2020-09-08 10:08:51 +0530

    Deleted

--- Json Parsing 2/Json Parsing 2/Objective-c Files/FirstVC.h
+++ Json Parsing 2/Json Parsing 2/Objective-c Files/FirstVC.h
@@ -11,14 +11,5 @@
 
 @interface FirstVC : UIViewController<UITableViewDelegate,UITableViewDataSource>
 
-//@property (strong, nonatomic)  UILabel *LblId;
-//@property (strong, nonatomic)  UILabel *LblName;
-//@property (strong, nonatomic)  UILabel *LblSalary;
-//@property (strong, nonatomic)  UILabel *LblAge;
-//@property (strong, nonatomic)  UIView *view;
-
-
-//@property (weak, nonatomic) IBOutlet UITableView *EmployeeDataTableView;
-//@property (nonatomic,strong) NSMutableArray<EmployeeDetails*> *arrEmployee;
 
 @end
