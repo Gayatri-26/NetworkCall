078f696245d3707f50b1d6829c09c9e6a235737d
Sonali Nivangune <nivangunesonali19@gmail.com>
2020-09-11 10:57:09 +0530

    new changes

--- Json Parsing 2/Json Parsing 2/Model/PersonDetail.m
+++ Json Parsing 2/Json Parsing 2/Model/PersonDetail.m
@@ -9,32 +9,38 @@
 #import "PersonDetail.h"
 #import "SecondVC.h"
 
+#import "PersonDetail.h"
+
 @implementation PersonDetail
 
 @synthesize Pid, Pname, Pemail, Paddress, Pgender;
-+(NSMutableArray < PersonDetail *> *)modelArrayFromDict:(NSDictionary *) d {
 
-   for (NSDictionary *dict in _PersonData)
-   {
-       PersonDetail *PersonDet = [[PersonDetail alloc]init];
-       
-       NSString *strid = [dict valueForKey:@"id"];
-       PersonDet.Pid = strid;
-       
-       NSString *strname = [dict valueForKey:@"name"];
-       PersonDet.Pname = strname;
-       
-       NSString *stremail = [dict valueForKey:@"email"];
-       PersonDet.Pemail= stremail;
-       
-       NSString *straddress = [dict valueForKey:@"address"];
-       PersonDet.Paddress = straddress;
-       
-       NSString *strgender = [dict valueForKey:@"gender"];
-       PersonDet.Pgender = strgender;
-       
-   }
-        }
-    } return emparr;
+
++(NSMutableArray < PersonDetail * > *)modelArrayFromDict:(NSDictionary *) d{
+    
+NSMutableArray *personDetailArr = [[NSMutableArray alloc]init];
+NSDictionary *dic1 = [d objectForKey:@"contacts"];
+   
+for (NSDictionary *dict in dic1)
+{
+    PersonDetail *PersonDet = [[PersonDetail alloc]init];
+    NSString *strid = [dict objectForKey:@"id"];
+    PersonDet.Pid = strid;
+    
+    NSString *strname = [dict objectForKey:@"name"];
+    PersonDet.Pname = strname;
+    
+    NSString *stremail = [dict objectForKey:@"email"];
+    PersonDet.Pemail= stremail;
+    
+    NSString *straddress = [dict objectForKey:@"address"];
+    PersonDet.Paddress = straddress;
+    
+    NSString *strgender = [dict objectForKey:@"gender"];
+    PersonDet.Pgender = strgender;
+    [personDetailArr addObject:PersonDet];
+   
+ }
+    return personDetailArr;
 }
 @end
